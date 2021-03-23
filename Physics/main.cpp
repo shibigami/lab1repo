2dbc5acc0771efdf64e4aa716648d5b67888eb1a
vitor manuel simplicio nunes <vitorpodeser@gmail.com>
2021-03-23 09:15:35 +0000

    Physics complete
    Completed physics

--- Physics/main.cpp
+++ Physics/main.cpp
@@ -1,9 +1,158 @@
 #include <Box2D/Box2D.h>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+constexpr uint16_t gameWidth = 800;
+constexpr uint16_t gameHeight = 600;
+
+//game window
+sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Physics");
+
 b2World* world;
+std::vector<b2Body*> bodies;
+std::vector<RectangleShape*> sprites;
+
+
+//1 sfml = 30 physics units
+const float physics_scale = 30.0f;
+//inverse of physics_scale, useful for calculations
+const float physics_scale_inv = 1.0f / physics_scale;
+//Magic numbers for accuracy of physics simulation
+const int32 velocityIterations = 6;
+const int32 positionIterations = 2;
+
+//Convert from b2Vec2 to a Vector2f
+inline const Vector2f bv2_to_sv2(const b2Vec2& in) {
+	return Vector2f(in.x * physics_scale, (in.y * physics_scale));
+}
+//Convert from Vector2f to a b2Vec2
+inline const b2Vec2 sv2_to_bv2(const Vector2f& in) {
+	return b2Vec2(in.x * physics_scale_inv, (in.y * physics_scale_inv));
+}
+//Convert from screenspace.y to physics.y (as they are the other way around)
+inline const Vector2f invert_height(const Vector2f& in) {
+	return Vector2f(in.x, gameHeight - in.y);
+}
+
+//Create a Box2D body with a box fixture
+b2Body* CreatePhysicsBox(b2World& World, const bool dynamic, const Vector2f& position, const Vector2f& size) {
+	b2BodyDef BodyDef;
+	//Is Dynamic(moving), or static(Stationary)
+	BodyDef.type = dynamic ? b2_dynamicBody : b2_staticBody;
+	BodyDef.position = sv2_to_bv2(position);
+	//Create the body
+	b2Body* body = World.CreateBody(&BodyDef);
+
+	//Create the fixture shape
+	b2PolygonShape Shape;
+	Shape.SetAsBox(sv2_to_bv2(size).x * 0.5f, sv2_to_bv2(size).y * 0.5f);
+	b2FixtureDef FixtureDef;
+	//Fixture properties
+	FixtureDef.density = dynamic ? 10.f : 0.f;
+	FixtureDef.friction = dynamic ? 0.8f : 1.f;
+	FixtureDef.restitution = 1.0f;
+	FixtureDef.shape = &Shape;
+	//Add to body
+	body->CreateFixture(&FixtureDef);
+	return body;
+}
+
+// Create a Box2d body with a box fixture, from a sfml::RectangleShape
+b2Body* CreatePhysicsBox(b2World& world, const bool dynamic, const RectangleShape& rs) {
+	return CreatePhysicsBox(world, dynamic, rs.getPosition(), rs.getSize());
+}
 
 void init() {
 	const b2Vec2 gravity(0.0f, -10.0f);
 
 	//Construct a world, which holds and simulates the physics bodies
 	world = new b2World(gravity);
+
+	// Wall Dimensions
+	Vector2f walls[] = {
+		// Top
+		Vector2f(gameWidth * .5f, 5.f), Vector2f(gameWidth, 10.f),
+		// Bottom
+		Vector2f(gameWidth * .5f, gameHeight - 5.f), Vector2f(gameWidth, 10.f),
+		// left
+		Vector2f(5.f, gameHeight * .5f), Vector2f(10.f, gameHeight),
+		// right
+		Vector2f(gameWidth - 5.f, gameHeight * .5f), Vector2f(10.f, gameHeight)
+	};
+
+	// Build Walls
+	for (int i = 0; i < 7; i += 2) {
+		// Create SFML shapes for each wall
+		auto s = new RectangleShape();
+		s->setSize(walls[i+1]);
+		s->setOrigin(walls[i]);
+		s->setPosition(walls[i]);
+		s->setFillColor(Color::White);
+		sprites.push_back(s);
+		// Create a static physics body for the wall
+		auto b = CreatePhysicsBox(*world, false, *s);
+		bodies.push_back(b);
+	}
+
+	// Create Boxes
+	for (int i = 1; i < 11; ++i) {
+		// Create SFML shapes for each box
+		auto s = new RectangleShape();
+		s->setPosition(Vector2f(i * (gameWidth / 12.f), gameHeight * .7f));
+		s->setSize(Vector2f(50.0f, 50.0f));
+		s->setOrigin(Vector2f(25.0f, 25.0f));
+		s->setFillColor(Color::White);
+		sprites.push_back(s);
+
+		// Create a dynamic physics body for the box
+		auto b = CreatePhysicsBox(*world, true, *s);
+		// Give the box a spin
+		b->ApplyAngularImpulse(5.0f, true);
+		bodies.push_back(b);
+	}
+}
+
+void Update() {
+	static sf::Clock clock;
+	float dt = clock.restart().asSeconds();
+
+	// Step Physics world by dt (non-fixed timestep) - THIS DOES ALL THE ACTUAL SIMULATION, DON'T FORGET THIS!
+	world->Step(dt, velocityIterations, positionIterations);
+
+	for (int i = 0; i < bodies.size(); ++i) {
+		// Sync Sprites to physics position
+		sprites[i]->setPosition(invert_height(bv2_to_sv2(bodies[i]->GetPosition())));
+		// Sync Sprites to physics Rotation
+		sprites[i]->setRotation((180 / b2_pi) * bodies[i]->GetAngle());
+	}
+}
+
+void Render() 
+{
+	for each ( auto& sprite in sprites)
+	{
+		window.draw(*sprite);
+	}
+}
+
+
+int main() 
+{
+	//enable vertical sync
+	window.setVerticalSyncEnabled(true);
+	bool close = false;
+	while (!close) 
+	{
+		if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) close = true;
+
+		window.clear();
+
+		Update();
+		Render();
+
+		window.display();
+	}
+	window.close();
 }
